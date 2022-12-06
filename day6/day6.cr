dd5eff2d4ee9a43efbfb713dfa5e571309e25db3
Nick Burns <nick@burns.io>
2022-12-05 21:38:42 -0800

    day 6 part 1

---
+++ day6/day6.cr
@@ -0,0 +1,18 @@
+#!/usr/bin/env crystal
+input = File.read("input.txt").strip.split('\n')
+
+TheBuffer = 4.times.map { |_| "" }.to_a
+
+def start?
+    TheBuffer.uniq.size == TheBuffer.size && TheBuffer.all? { |e| e != "" }
+end
+
+input.each do |line|
+    line.split("").each_with_index(1) do |c, i|
+        TheBuffer[i % TheBuffer.size] = c
+        if start?
+            puts i
+            exit
+        end
+    end
+end
